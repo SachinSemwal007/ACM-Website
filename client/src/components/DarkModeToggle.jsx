b09cedaf8975bbe3e3566cccf4387bc90edac82c
Shivam <139750193+Shivvy-The-Coder@users.noreply.github.com>
2025-06-27 16:25:39 +0530

    navbar and footer icon updted

--- client/src/components/DarkModeToggle.jsx
+++ client/src/components/DarkModeToggle.jsx
@@ -20,7 +20,8 @@ const DarkModeToggle = () => {
   return (
     <button
       onClick={() => setDarkMode(prev => !prev)}
-      className="p-2 px-4 rounded border bg-white dark:bg-gray-700 text-black dark:text-white shadow"
+      // className="p-2 px-4 rounded border bg-white dark:bg-gray-700 text-black dark:text-white shadow"
+      className="p-2 px-4 rounded  bg-gray-400/10  text-black  hover:bg-amber-200"
     >
       {darkMode ? '☀️' : '🌙'}
     </button>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Menu, X, Search } from "lucide-react";
-import logo from "../assets/ACM.png";
+import logo from "../assets/ACM2.png";
 import DarkModeToggle from "./DarkModeToggle";
 
 const Navbar = () => {
@@ -10,25 +10,23 @@ const Navbar = () => {
     <nav className="bg-white  shadow-md transition-colors">
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-20 items-center">
-
-{/* logo  */}
+          {/* Logo */}
           <div className="flex-shrink-0">
-            <img src={logo} alt="ACM" className="h-10 w-auto" />
+            <img src={logo} alt="ACM" className="h-15 w-auto" />
           </div>
 
-         
           {/* Navigation Links */}
-          <div className="hidden md:flex space-x-6 w-full pl-10 lg:px-10 ">
-            <a href="#" className="text-black hover:text-blue-900 lg:text-[15px] text-[13px]">
+          <div className="hidden md:flex space-x-6 w-full pl-7 lg:pl-16 ">
+            <a href="#" className="text-black hover:text-blue-900 lg:text-[16px] text-[14px]">
               PRODUCT
             </a>
-            <a href="#" className="text-black hover:text-blue-900 lg:text-[15px] text-[13px]">
+            <a href="#" className="text-black hover:text-blue-900 lg:text-[16px] text-[14px]">
               SUPPORT
             </a>
-            <a href="#" className="text-black hover:text-blue-900 lg:text-[15px] text-[13px]">
+            <a href="#" className="text-black hover:text-blue-900 lg:text-[16px] text-[14px]">
               SOLUTION
             </a>
-            <a href="#" className="text-black hover:text-blue-900 lg:text-[15px] text-[13px]">
+            <a href="#" className="text-black hover:text-blue-900 lg:text-[16px] text-[14px]">
               ABOUT US
             </a>
           </div>
@@ -39,22 +37,23 @@ const Navbar = () => {
               <input
                 type="text"
                 placeholder="Search..."
-                className="pl-3 pr-10 py-2 w-48 lg:w-56 border border-gray-300 dark:border-gray-600 rounded-md bg-white dark:bg-gray-800 text-black dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+                // className="pl-3 pr-10 py-2 w-48 lg:w-56 border border-gray-300 dark:border-gray-600 rounded-md bg-white dark:bg-gray-800 text-black dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="pl-3 pr-10 py-2 w-48 lg:w-56 border border-gray-300  rounded-md bg-white  text-black  focus:outline-none focus:ring-2 focus:ring-blue-500/50"
               />
               <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 dark:text-gray-300" size={18} />
             </div>
 
-           
             <div className="flex-shrink-0">
               <DarkModeToggle />
             </div>
           </div>
 
-{/* mobile menu icon */}
-          <div className="md:hidden">
+    {/* {hamburger icon} */}
+
+          <div className="md:hidden flex-shrink-0">
             <button
               onClick={() => setIsOpen(!isOpen)}
-              className="text-gray-700 dark:text-white focus:outline-none"
+              className="text-gray-900 dark:text-gray-900 focus:outline-none p-2 hover:bg-gray-900 dark:hover:bg-gray-800 rounded-md border border-gray-900 dark:border-gray-900"
             >
               {isOpen ? <X size={24} /> : <Menu size={24} />}
             </button>
@@ -62,21 +61,22 @@ const Navbar = () => {
         </div>
       </div>
 
-      {/* Mobile Menu */}
+      {/* for Mobile Menu */}
       {isOpen && (
-        <div className="md:hidden px-4 pb-4">
-          <div className="flex flex-col space-y-2">
-            <a href="#" className="text-gray-700 hover:text-blue-900 lg:text-[18px] text-[14px]">
-              Product
+        // <div className="md:hidden px-4 pb-4 bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-700">
+        <div className="md:hidden px-4 pb-4 bg-white  border-t border-gray-200 ">
+          <div className="flex flex-col space-y-3 pt-3">
+            <a href="#" className="text-gray-700  hover:text-blue-900 text-[16px] py-2 border-1 rounded-md px-3">
+              PRODUCT
             </a>
-            <a href="#" className="text-gray-700 dark:text-white hover:text-blue-600">
-              Support
+            <a href="#" className="text-gray-700  hover:text-blue-600 text-[16px] py-2 border-1 rounded-md px-3">
+              SUPPORT
             </a>
-            <a href="#" className="text-gray-700 dark:text-white hover:text-blue-600">
-              Solution
+            <a href="#" className="text-gray-700  hover:text-blue-600 text-[16px] py-2 border-1 rounded-md px-3">
+              SOLUTION
             </a>
-            <a href="#" className="text-gray-700 dark:text-white hover:text-blue-600">
-              About Us
+            <a href="#" className="text-gray-700  hover:text-blue-600 text-[16px] py-2 border-1 rounded-md px-3">
+              ABOUT US
             </a>
             <div className="flex mt-3 items-center gap-2">
               <div className="relative flex-1">
@@ -89,9 +89,11 @@ const Navbar = () => {
               </div>
             </div>
 
-{/* otpional toggke menu */}
+
+
             <div className="mt-3">
               <DarkModeToggle />
+
             </div>
           </div>
         </div>
